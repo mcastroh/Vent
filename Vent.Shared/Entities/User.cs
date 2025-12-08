bfb67aa96d07d1d93c74fb2f757becc427cc4636
mcastroh <m_castrohinostroza@hotmail.com>
2025-12-08 09:29:48 -0500

    add entity User

---
+++ Vent.Shared/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vent.Shared.Entities;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Display(Name = "Usuario")]
+    [MaxLength(60, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public string Name { get; set; } = null!;
+}
